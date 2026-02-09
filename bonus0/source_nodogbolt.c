6b7884c16bd31c41ec2d8663e8ccbf266723646e
aboulore <aboulore@sudent.42.fr>
2026-02-09 17:02:04 +0100

    source_nodogbolt.c compile

--- bonus0/source_nodogbolt.c
+++ bonus0/source_nodogbolt.c
@@ -1,26 +1,43 @@
 #include <stdio.h>
+#include <string.h>
+#include <unistd.h>
 
 char tiret[] = " - ";
 
-void p(char *v, char *tiret) {
-	puts(tiret);
 
-	read(0, v, 0x1000);
+// 1ere occurence	:	v(v1) = 0xbffff6b8
+// 2nde occurence	:	v(v2) = 0xbffff6cb
+void p(char *v, char *tiret) {
+	char buf[0x1000];				// buf = 0xbfffe680
+	puts(tiret);
+	read(0, buf, 0x1000);
+	
+	// mettre un \0 a la place du \n dans buf
+	char *back_0 = strchr(buf, '\n');
+	*back_0 = 0;
+	strncpy(v,buf, 20);
+	return ;
 }
 
-void pp(char *ptr) {
-	char *v1;
-	char *v2;
+// bp = 0xbffff6e8
+// bp + 4 = 0xbffff6ec(eip -> 0x080485b9)
+void pp(char *v0) {
+	char v1[20];
+	char v2[20];
 
 	p(v1, tiret);
 	p(v2, tiret);
-	
-
+	strcpy(v0, v1);
+	v0[strlen(v1)] = ' ';
+	v0[strlen(v1)+1] = '\0';
+	strcat(v0, v2);
+	return ;
 }
 
+// bp = 0xbffff738
 int main(void) {
-	char buf[0x40-0x16];
-	pp(buf);
-	puts(buf);
+	char v0[0x2A];		// v0 = 0xbffff706
+	pp(v0);
+	puts(v0);
 	return (0);
 }
